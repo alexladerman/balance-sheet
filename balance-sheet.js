822e9ae502302202de38cce60a88bc69b0fb1530
Alexander Laderman <alexladerman@gmail.com>
2016-04-21 00:45:58 +0200

    upload commit

---
+++ balance-sheet.js
@@ -0,0 +1,161 @@
+var elements = [{"accountNumber":1,"accountType":"heading","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"FINANCIACION BASICA","accountingYears":"http://localhost:81/accounts/1/accounting-years","self":"http://localhost:81/accounts/1"}
+,{"accountNumber":10,"accountType":"heading","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"CAPITAL","accountingYears":"http://localhost:81/accounts/10/accounting-years","self":"http://localhost:81/accounts/10"}
+,{"accountNumber":100,"accountType":"sumAlpha","balance":-2906.06,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Capital social","accountingYears":"http://localhost:81/accounts/100/accounting-years","self":"http://localhost:81/accounts/100"}
+,{"accountNumber":1000,"accountType":"status","balance":-2906.06,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"credit","name":"Capital social socio 1","accountingYears":"http://localhost:81/accounts/1000/accounting-years","self":"http://localhost:81/accounts/1000"}
+,{"accountNumber":1001,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"credit","name":"Capital social socio 2","accountingYears":"http://localhost:81/accounts/1001/accounting-years","self":"http://localhost:81/accounts/1001"}
+,{"accountNumber":101,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Fondo social","accountingYears":"http://localhost:81/accounts/101/accounting-years","self":"http://localhost:81/accounts/101"}
+,{"accountNumber":102,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Capital","accountingYears":"http://localhost:81/accounts/102/accounting-years","self":"http://localhost:81/accounts/102"}
+,{"accountNumber":103,"accountType":"sumAlpha","balance":-99,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Socios por desembolsos no exigidos","accountingYears":"http://localhost:81/accounts/103/accounting-years","self":"http://localhost:81/accounts/103"}
+,{"accountNumber":1030,"accountType":"status","balance":-99,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Socios por desembolsos no exigidos, capital social","accountingYears":"http://localhost:81/accounts/1030/accounting-years","self":"http://localhost:81/accounts/1030"}
+,{"accountNumber":1034,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Socios por desembolsos no exigidos, capital pendiente de inscripción","accountingYears":"http://localhost:81/accounts/1034/accounting-years","self":"http://localhost:81/accounts/1034"}
+,{"accountNumber":104,"accountType":"sumAlpha","balance":564,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Socios por aportaciones no dinerarias pendientes","accountingYears":"http://localhost:81/accounts/104/accounting-years","self":"http://localhost:81/accounts/104"}
+,{"accountNumber":1040,"accountType":"status","balance":564,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Socios por aportaciones no dinerarias pendientes, capital social","accountingYears":"http://localhost:81/accounts/1040/accounting-years","self":"http://localhost:81/accounts/1040"}
+,{"accountNumber":1044,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Socios por aportaciones no dinerarias pendientes, capital pendiente de insc","accountingYears":"http://localhost:81/accounts/1044/accounting-years","self":"http://localhost:81/accounts/1044"}
+,{"accountNumber":108,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Acciones o participaciones propias en situaciones especiales","accountingYears":"http://localhost:81/accounts/108/accounting-years","self":"http://localhost:81/accounts/108"}
+,{"accountNumber":109,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Acciones  o  participaciones  propias  para reducción de capital","accountingYears":"http://localhost:81/accounts/109/accounting-years","self":"http://localhost:81/accounts/109"}
+,{"accountNumber":11,"accountType":"heading","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"RESERVAS Y OTROS INSTRUMENTOS DE PATRIMONIO","accountingYears":"http://localhost:81/accounts/11/accounting-years","self":"http://localhost:81/accounts/11"}
+,{"accountNumber":110,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Prima de emisión o asunción","accountingYears":"http://localhost:81/accounts/110/accounting-years","self":"http://localhost:81/accounts/110"}
+,{"accountNumber":111,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Otros instrumentos de patrimonio neto","accountingYears":"http://localhost:81/accounts/111/accounting-years","self":"http://localhost:81/accounts/111"}
+,{"accountNumber":1110,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Patrimonio neto por emisión de instrumentos financieros compuestos","accountingYears":"http://localhost:81/accounts/1110/accounting-years","self":"http://localhost:81/accounts/1110"}
+,{"accountNumber":1111,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Resto de instrumentos de patrimonio neto","accountingYears":"http://localhost:81/accounts/1111/accounting-years","self":"http://localhost:81/accounts/1111"}
+,{"accountNumber":112,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reserva legal","accountingYears":"http://localhost:81/accounts/112/accounting-years","self":"http://localhost:81/accounts/112"}
+,{"accountNumber":113,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reservas voluntarias","accountingYears":"http://localhost:81/accounts/113/accounting-years","self":"http://localhost:81/accounts/113"}
+,{"accountNumber":114,"accountType":"sumAlpha","balance":-564,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reservas especiales","accountingYears":"http://localhost:81/accounts/114/accounting-years","self":"http://localhost:81/accounts/114"}
+,{"accountNumber":1140,"accountType":"status","balance":-564,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reservas para acciones o participaciones de la sociedad dominante","accountingYears":"http://localhost:81/accounts/1140/accounting-years","self":"http://localhost:81/accounts/1140"}
+,{"accountNumber":1141,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reservas estatutarias","accountingYears":"http://localhost:81/accounts/1141/accounting-years","self":"http://localhost:81/accounts/1141"}
+,{"accountNumber":1142,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reserva por capital amortizado","accountingYears":"http://localhost:81/accounts/1142/accounting-years","self":"http://localhost:81/accounts/1142"}
+,{"accountNumber":1143,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reserva por fondo de comercio","accountingYears":"http://localhost:81/accounts/1143/accounting-years","self":"http://localhost:81/accounts/1143"}
+,{"accountNumber":1144,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reservas por acciones propias aceptadas en garantía","accountingYears":"http://localhost:81/accounts/1144/accounting-years","self":"http://localhost:81/accounts/1144"}
+,{"accountNumber":115,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reservas por pérdidas y ganancias actuariales y otros ajustes","accountingYears":"http://localhost:81/accounts/115/accounting-years","self":"http://localhost:81/accounts/115"}
+,{"accountNumber":116,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reserva de revalorizaciones","accountingYears":"http://localhost:81/accounts/116/accounting-years","self":"http://localhost:81/accounts/116"}
+,{"accountNumber":117,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Reserva por transacciones con pagos basados en acciones","accountingYears":"http://localhost:81/accounts/117/accounting-years","self":"http://localhost:81/accounts/117"}
+,{"accountNumber":118,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Aportaciones de socios o propietarios","accountingYears":"http://localhost:81/accounts/118/accounting-years","self":"http://localhost:81/accounts/118"}
+,{"accountNumber":119,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Diferencias por ajuste del capital a euros","accountingYears":"http://localhost:81/accounts/119/accounting-years","self":"http://localhost:81/accounts/119"}
+,{"accountNumber":12,"accountType":"heading","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"RESULTADOS PENDIENTES DE APLICACIÓN","accountingYears":"http://localhost:81/accounts/12/accounting-years","self":"http://localhost:81/accounts/12"}
+,{"accountNumber":120,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Remanente","accountingYears":"http://localhost:81/accounts/120/accounting-years","self":"http://localhost:81/accounts/120"}
+,{"accountNumber":121,"accountType":"sumAlpha","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Resultados negativos de ejercicios anteriores","accountingYears":"http://localhost:81/accounts/121/accounting-years","self":"http://localhost:81/accounts/121"}
+,{"accountNumber":1210,"accountType":"status","balance":0,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":" Resultados negativos ejercicio 1","accountingYears":"http://localhost:81/accounts/1210/accounting-years","self":"http://localhost:81/accounts/1210"}
+,{"accountNumber":129,"accountType":"sumAlpha","balance":-32001.71,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"debit","name":"Resultado del ejercicio","accountingYears":"http://localhost:81/accounts/129/accounting-years","self":"http://localhost:81/accounts/129"}
+,{"accountNumber":1290,"accountType":"profitAndLoss","balance":-32001.71,"balanceDaybook":0,"blockDirectEntries":false,"debitCredit":"credit","name":"Resultado del ejercicio","accountingYears":"http://localhost:81/accounts/1290/accounting-years","self":"http://localhost:81/accounts/1290"}];
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function (searchString, position) {
+      position = position || 0;
+      return this.lastIndexOf(searchString, position) === position;
+    }
+  });
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function print(elements) {
+
+        var fieldMap = [
+                        {"numero": "accountNumber"},
+                        {"nombre": "name"},
+                        {"tipo": "debitCredit"},
+                        {"saldo": "balance"}
+                        ];
+
+
+//        sort(elements);
+
+        document.getElementById("elements").innerHTML = buildTable(elements, fieldMap);
+
+        summarize(elements);
+
+        document.getElementById("elements_traversed").innerHTML = buildTable(elements, fieldMap);
+}
+
+function buildTable(elements, fieldMap) {
+        var output = '<table border=0 cellspacing=0 cellpadding=2>';
+
+        output += '<tr>';
+        for (var i = 0; i < fieldMap.length; i++) {
+            var outKey = Object.keys(fieldMap[i])[0];
+            output += '<td>' + outKey + '</td>';
+        }
+        output += '</tr>';
+
+        for (var j = 0; j < elements.length; j++) {
+            output += '<tr>';
+            for (var i = 0; i < fieldMap.length; i++) {
+
+                var outKey = Object.keys(fieldMap[i])[0];
+                var inKey = fieldMap[i][outKey];
+                var value = elements[j][inKey];
+
+                    output += '<td>' + value + '</td>';
+            }
+            output += '</tr>';
+        }
+        output += '</table></body></html>';
+
+        return output;
+}
+
+function sort(elements) {
+    elements.sort(function (a, b) {
+//      console.log("sorting" + a.voucherNumber);
+        if (a.accountNumber.toString() > b.accountNumber.toString())
+          return 1;
+        if (a.accountNumber.toString() < b.accountNumber.toString())
+          return -1;
+        // a must be equal to b
+        return 0;
+    });
+
+}
+
+function summarize(elements) {
+    console.log("\nsummarize\n");
+    var len = elements.length;
+    var node, traversed;
+    for (var i = 0; i < len; i++) {
+        node = elements[i];
+        if (node["accountNumber"].toString().length == 2) {
+            traversed = traverse(node, i+1, elements, len);
+            i = traversed[1];
+        }
+    }
+}
+
+function traverse(node, i, elements, len) {
+    console.log("\ntraverse \n");
+    var nextNode, traversed, nodeAcct, nextNodeAcct;
+    nodeAcct = node["accountNumber"].toString();
+    var sumSiblings = node["balance"];
+    while (i < len) {
+        nextNode = elements[i];
+        if (nextNode) {
+            nextNodeAcct = nextNode["accountNumber"].toString();
+            if (nextNodeAcct.length > nodeAcct.length ) { //if is a childNode
+                 traversed = traverse(nextNode, i+1, elements, len);
+                 sumSiblings += traversed[0]-node["balance"];
+                 node["balance"] = traversed[0];
+                 i = traversed[1];
+            }
+            else if (nextNodeAcct.length == nodeAcct.length) {
+                 node = nextNode;
+                 nodeAcct = nextNodeAcct;
+                 sumSiblings += node["balance"]
+                 ++i;
+            }
+            else
+                return [sumSiblings, i];
+        }
+    }
+    return [sumSiblings, i];
+}
+
+window.onload = function() {
+    // all of your code goes in here
+    // it runs after the DOM is built
+    print(elements);
+}
